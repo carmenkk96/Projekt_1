a030139075d63ced974f59120c10b3850481081c
ahtojarve <jarveahto@gmail.com>
2023-04-02 14:58:43 +0300

    Panin selle tööle nii, et ta teeb faili, kus säilitab ID-de listi. ChatGpt ja google ütlesid, et see peaks .ser lõpuga fail olema (hea tava). Tänu sellele listile tegin palju muutusi ja kogu see värk ei ole enam nii ilus, kui algses variandis.

--- src/TaotluseGenereerimine.java
+++ src/TaotluseGenereerimine.java
@@ -1,7 +1,10 @@
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
 import java.util.Scanner;
 
-//uus branch
+
 public class TaotluseGenereerimine {
 
     public static void main(String[] args) throws Exception {
@@ -13,8 +16,25 @@ public class TaotluseGenereerimine {
         String tooteNimi = scan.nextLine();
 
         //lisame uued andmed uute taotluse isendisse
-        Taotlus uusTaotlus = new ID(taotlejaNimi, tooteNimi);
-        System.out.println(uusTaotlus);
+        ID uusId = new ID(taotlejaNimi, tooteNimi);
+        Taotlus uusTaotlus = new Taotlus(taotlejaNimi, tooteNimi);
+        System.out.println(uusId);
+
+        //Lihtsalt testimiseks näitan ID listi, mis on salvestatud
+        //EI OLE PROGRAMMI TOIMIMISEKS VAJALIK!
+        try {
+            FileInputStream fileIn = new FileInputStream("idlist.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            List<String> list = (List<String>) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Testimiseks näitan ID listi, mis on salvestatud: " + list);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        uusTaotlus.taotluseKirjutamineFaili(uusId.getId());
+        //Kirjutame uue taotluse andmebaasi
+
 
     }
 }
